68bf6afd5cd4c67cee5b80b10ae1cd6ac420b0e5
Ricardo Hernandez <richhquim@gmail.com>
2025-09-22 19:21:09 -0600

    claves

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,14 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBk1VwgnXGm3xsg34AUAu474c4TVny3NFE",
-    authDomain: "lapostriza-project.firebaseapp.com",
-    projectId: "lapostriza-project",
-    storageBucket: "lapostriza-project.firebasestorage.app",
-    messagingSenderId: "148874867471",
-    appId: "1:148874867471:web:e0018e3573fa1ce1f1847a",
-    measurementId: "G-WZLQMDR33X"
+    // Si la variable de Vite existe, úsala. Si no, usa la de Vercel.
+    // Esto hace el código compatible con ambos entornos.
+    apiKey: import.meta.env.VITE_API_KEY || process.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN || process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID || process.env.FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET || process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID || process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
